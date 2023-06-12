5d5a43eafad13f83524128317e1695785f5d0b1e
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-12 15:49:18 +0200

    Add: basic router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index-js/index'
 import store from './store/store'
+import router from './router/Router'
+
+
 
 createApp(App)
 .use(store)
+.use(router)
 .mount('#app')
