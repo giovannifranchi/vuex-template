a3a8dc9a2347c0ecf05b0e6dc118272c6336e7b6
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-15 00:25:36 +0200

    Add: fontawesome

--- src/components/utils/CardComponent.vue
+++ src/components/utils/CardComponent.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
     export default {
         name: 'CardComponent'
     }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 import './assets/index-js/index'
 import store from './store/store'
 import router from './router/Router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fa5 } from '@fortawesome/free-solid-svg-icons' //this is just an example, each new icon used needs to be registered here;
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fa5); //icons must be registered also here, remeber to use them in components without fa-regular, fa-solid, fa-light etc...
 
 
 
 createApp(App)
 .use(store)
 .use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
 .mount('#app')
