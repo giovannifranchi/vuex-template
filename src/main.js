3e3d46d649dee9ca1aac1a9269d4a3acd6549cc7
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-15 00:31:33 +0200

    Add: apexcharts

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import './assets/index-js/index'
 import store from './store/store'
 import router from './router/Router'
+
+//exetrnal components
+
+import VueApexCharts from 'vue3-apexcharts' //this is a library for charts, find docs @'https://apexcharts.com/docs/vue-charts/'...
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fa5 } from '@fortawesome/free-solid-svg-icons' //this is just an example, each new icon used needs to be registered here;
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -15,4 +19,5 @@ createApp(App)
 .use(store)
 .use(router)
 .component('font-awesome-icon', FontAwesomeIcon)
+.component('VueApexCharts', VueApexCharts)
 .mount('#app')
